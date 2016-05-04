f2c68002f1251585f81ee468dd43ec0763c3a933
Ionitron <hi@ionicframework.com>
2016-05-04 15:58:17 -0500

    testing a thing

---
+++ test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ ! -d "~/ionic-site" ]; then
+  echo "checking out"
+  echo "-- Cloning ionic-site#master to ~/ionic-site..."
+
+  git config --global user.email "hi@ionicframework.com"
+  git config --global user.name "Ionitron"
+
+  ARGS=" --depth=3"
+  git clone git@github.com:driftyco/ionic-site.git ~/ionic-site $ARGS
+  cd ~/ionic-site
+  git fetch origin --tags
+else
+  echo "using existing"
+  cd ~/ionic-site
+  git reset --hard
+  git pull origin master
+fi
+
+echo "Test 1" > test.txt
+
+
+git add -A
+git commit -am "Testing"
+git push origin master
